939f549814beb05e70de090f78a7929944063375
Joshua0926 <98793837+Joshua0926@users.noreply.github.com>
2022-02-26 23:21:26 -0600

    Add files via upload

---
+++ Tarea2/src/datosTarea.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RANDY
+ */
+public class datosTarea {
+    
+    private String nombre;
+    private String contraseña;
+    
+    datosTarea(String nombre, String contraseña){
+        this.nombre = "Joshua";
+        this.contraseña = "202110773";
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}
